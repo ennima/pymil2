2b599bf392edfc42ada66840f21db2c4d9353638
ennima <ennima@hotmail.com>
2015-09-18 22:42:34 -0500

    live2

--- play.py
+++ play.py
@@ -1,9 +1,10 @@
 import os
+import subprocess
 print("Hola")
 print("Playing...")
 
-play = os.sys("livestreamer hlsvariant://multimedioshls-i.akamaihd.net/hls/live/223118/57828478001/master.m3u8 best --player omxplayer --fifo player-passthrough hls")
-if(play == 0):
-	print "Mamo"
-else:
-	print "Cool"
+#play = os.sys("livestreamer hlsvariant://multimedioshls-i.akamaihd.net/hls/live/223118/57828478001/master.m3u8 best --player omxplayer --fifo player-passthrough hls")
+cmd = "livestreamer hlsvariant://multimedioshls-i.akamaihd.net/hls/live/223118/57828478001/master.m3u8 best --player omxplayer --fifo player-passthrough hls"
+process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+process.wait()
+print process.returncode
