c60e177c72f0f8cb31c8c41aa7b39faeb964df15
ennima <ennima@hotmail.com>
2015-09-18 22:20:22 -0500

    live

--- play.py
+++ play.py
@@ -1,3 +1,9 @@
 import os
 print("Hola")
-print("d")
+print("Playing...")
+
+play = os.sys("livestreamer hlsvariant://multimedioshls-i.akamaihd.net/hls/live/223118/57828478001/master.m3u8 best --player omxplayer --fifo player-passthrough hls")
+if(play == 0):
+	print "Mamo"
+else:
+	print "Cool"
